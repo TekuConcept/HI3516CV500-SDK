9d918a385106f9c0a214d279a6705155c9014093
TekuConcept <electro_madness@yahoo.com>
2021-04-26 12:03:12 -0600

    Dev Checkpoint 202104261203

--- sdk/libraries/mpi/mpi_gdc.c
+++ sdk/libraries/mpi/mpi_gdc.c
@@ -3,123 +3,117 @@
  */
 
 #include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #include "re_mpi_comm.h"
 #include "re_mpi_gdc.h"
 
+#define GDC_DEV_NAME "/dev/gdc"
+
+// ============================================================================
+
 pthread_mutex_t g_gdc_mutex;
 HI_S32          g_gdc_mpi_fd = -1;
+
+// ============================================================================
 
 HI_S32
 mpi_gdc_check_open()
 {
     pthread_mutex_lock(&g_gdc_mutex);
 
-    if (g_gdc_mpi_fd == -1 &&
-        (g_gdc_mpi_fd = open("/dev/gdc", 2, 0), g_gdc_mpi_fd == -1)) {
-        pthread_mutex_unlock(&g_gdc_mutex);
-        printf("func %s open /dev/gdc failed! \n", __FUNCTION__);
-        return ERR_GDC_SYS_NOTREADY;
+    if ( g_gdc_mpi_fd < 0 ) {
+        g_gdc_mpi_fd = open(GDC_DEV_NAME, O_RDWR, 0);
+        if ( g_gdc_mpi_fd < 0 ) {
+            pthread_mutex_unlock(&g_gdc_mutex);
+            printf("func %s open /dev/gdc failed! \n", __FUNCTION__);
+            return HI_ERR_GDC_SYS_NOTREADY;
+        }
     }
 
     pthread_mutex_unlock(&g_gdc_mutex);
-    return 0;
+    return HI_SUCCESS;
 }
 
 HI_S32
-hi_mpi_gdc_add_pmf_task(
-    GDC_HANDLE             hHandle,
-    const GDC_TASK_ATTR_S* pstTask,
-    const GDC_PMF_ATTR_S*  pstGdcPmfAttr)
+hi_mpi_gdc_add_pmf_task(GDC_HANDLE hHandle, const GDC_TASK_ATTR_S *pstTask, const GDC_PMF_ATTR_S *pstGdcPmfAttr)
 {
-    GDC_PMF_TASK_INFO_S data;
+    HI_S32 result;
+    GDC_PMF_TASK_INFO_S stPmfInfo;
 
-    memset(&data, 0, sizeof(data));
+    memset(&stPmfInfo, 0, sizeof(stPmfInfo));
 
-    if (mpi_gdc_check_open()) return ERR_GDC_SYS_NOTREADY;
+    if ( mpi_gdc_check_open() != HI_SUCCESS )
+        return HI_ERR_GDC_SYS_NOTREADY;
 
-    if (pstTask && pstGdcPmfAttr) {
-        data.hHandle = hHandle;
-        memcpy_s(
-            &data.stTask,
-            sizeof(GDC_TASK_ATTR_S),
-            pstTask,
-            sizeof(GDC_TASK_ATTR_S));
-        memcpy_s(
-            &data.stGdcPmfAttr,
-            sizeof(GDC_PMF_ATTR_S),
-            pstGdcPmfAttr,
-            sizeof(GDC_PMF_ATTR_S));
-        return ioctl(g_gdc_mpi_fd, 0x45404E05u, &data);
-    }
-    else {
+    if ( pstTask == HI_NULL || pstGdcPmfAttr == HI_NULL ) {
         printf("func %s NULL pointer detected\n", __FUNCTION__);
-        return ERR_GDC_NULL_PTR;
+        return HI_ERR_GDC_NULL_PTR;
     }
+
+    stPmfInfo.hHandle = hHandle;
+    memcpy_s(
+        &stPmfInfo.stTask, sizeof(GDC_TASK_ATTR_S),
+        pstTask, sizeof(GDC_TASK_ATTR_S));
+    memcpy_s(
+        &stPmfInfo.stGdcPmfAttr, sizeof(GDC_PMF_ATTR_S),
+        pstGdcPmfAttr, sizeof(GDC_PMF_ATTR_S));
+    return ioctl(g_gdc_mpi_fd, IOC_GDC_ADD_PMF_TASK, &stPmfInfo);
 }
 
 HI_S32
-hi_mpi_gdc_fisheye_pos_query_dst_to_src(
-    const GDC_FISHEYE_POINT_QUERY_ATTR_S* pstFisheyePointQueryAttr,
-    const VIDEO_FRAME_INFO_S*             pstVideoInfo,
-    const POINT_S*                        pstDstPoint,
-    POINT_S*                              pstSrcPoint)
-{
-    return ERR_GDC_NOT_SUPPORT;
-}
+hi_mpi_gdc_fisheye_pos_query_dst_to_src(const GDC_FISHEYE_POINT_QUERY_ATTR_S *pstFisheyePointQueryAttr, const VIDEO_FRAME_INFO_S *pstVideoInfo, const POINT_S *pstDstPoint, POINT_S *pstSrcPoint)
+{ return HI_ERR_GDC_NOT_SUPPORT; }
 
 HI_S32
-HI_MPI_GDC_BeginJob(GDC_HANDLE* phHandle)
+HI_MPI_GDC_BeginJob(GDC_HANDLE *phHandle)
 {
-    if (mpi_gdc_check_open()) return ERR_GDC_SYS_NOTREADY;
+    if ( mpi_gdc_check_open() != HI_SUCCESS )
+        return HI_ERR_GDC_SYS_NOTREADY;
 
-    if (phHandle) return ioctl(g_gdc_mpi_fd, 0x80044E00, phHandle);
+    if ( phHandle == HI_NULL ) {
+        printf("func %s NULL pointer detected\n", __FUNCTION__);
+        return HI_ERR_GDC_NULL_PTR;
+    }
 
-    printf("func %s NULL pointer detected\n", __FUNCTION__);
-    return ERR_GDC_NULL_PTR;
-}
-
-HI_S32
-HI_MPI_GDC_EndJob(GDC_HANDLE hHandle)
-{
-    if (mpi_gdc_check_open())
-        return ERR_GDC_SYS_NOTREADY;
-    else
-        return ioctl(g_gdc_mpi_fd, 0x40044E02u, &hHandle);
+    return ioctl(g_gdc_mpi_fd, IOC_GDC_BEGIN_JOB, phHandle);
 }
 
 HI_S32
 HI_MPI_GDC_CancelJob(GDC_HANDLE hHandle)
 {
-    if (mpi_gdc_check_open())
-        return ERR_GDC_SYS_NOTREADY;
-    else
-        return ioctl(g_gdc_mpi_fd, 0x40044E01u, &hHandle);
+    if ( mpi_gdc_check_open() != HI_SUCCESS )
+        return HI_ERR_GDC_SYS_NOTREADY;
+    return ioctl(g_gdc_mpi_fd, IOC_GDC_CANCEL_JOB, &hHandle);
 }
 
 HI_S32
-HI_MPI_GDC_SetConfig(
-    GDC_HANDLE                  hHandle,
-    const FISHEYE_JOB_CONFIG_S* pstJobConfig)
+HI_MPI_GDC_EndJob(GDC_HANDLE hHandle)
 {
-    return ERR_GDC_NOT_SUPPORT;
+    if ( mpi_gdc_check_open() != HI_SUCCESS )
+        return HI_ERR_GDC_SYS_NOTREADY;
+    return ioctl(g_gdc_mpi_fd, IOC_GDC_END_JOB, &hHandle);
 }
 
 HI_S32
-HI_MPI_GDC_AddCorrectionTask(
-    GDC_HANDLE             hHandle,
-    const GDC_TASK_ATTR_S* pstTask,
-    const FISHEYE_ATTR_S*  pstFisheyeAttr)
-{
-    return ERR_GDC_NOT_SUPPORT;
-}
+HI_MPI_GDC_SetConfig(GDC_HANDLE hHandle, const FISHEYE_JOB_CONFIG_S *pstJobConfig)
+{ return HI_ERR_GDC_NOT_SUPPORT; }
 
 HI_S32
-HI_MPI_GDC_AddCorrectionExTask(
-    GDC_HANDLE               hHandle,
-    const GDC_TASK_ATTR_S*   pstTask,
-    const FISHEYE_ATTR_EX_S* pstFishEyeAttrEx,
-    HI_BOOL                  bCheckMode)
-{
-    return ERR_GDC_NOT_SUPPORT;
-}
+HI_MPI_GDC_AddCorrectionTask(GDC_HANDLE hHandle, const GDC_TASK_ATTR_S *pstTask, const FISHEYE_ATTR_S *pstFisheyeAttr)
+{ return HI_ERR_GDC_NOT_SUPPORT; }
+
+HI_S32
+HI_MPI_GDC_AddCorrectionExTask(GDC_HANDLE hHandle, const GDC_TASK_ATTR_S *pstTask, const FISHEYE_ATTR_EX_S *pstFishEyeAttrEx, HI_BOOL bCheckMode)
+{ return HI_ERR_GDC_NOT_SUPPORT; }
+
+HI_S32
+HI_MPI_GDC_AddPMFTask(GDC_HANDLE hHandle, const GDC_TASK_ATTR_S *pstTask, const GDC_PMF_ATTR_S *pstGdcPmfAttr)
+{ return hi_mpi_gdc_add_pmf_task(hHandle, pstTask, pstGdcPmfAttr); }
+
+HI_S32
+HI_MPI_GDC_FisheyePosQueryDst2Src(const GDC_FISHEYE_POINT_QUERY_ATTR_S *pstFisheyePointQueryAttr, const VIDEO_FRAME_INFO_S *pstVideoInfo, const POINT_S *pstDstPoint, POINT_S *pstSrcPoint)
+{ return hi_mpi_gdc_fisheye_pos_query_dst_to_src(pstFisheyePointQueryAttr, pstVideoInfo, pstDstPoint, pstSrcPoint); }
--- sdk/libraries/mpi/re_mpi_gdc.h
+++ sdk/libraries/mpi/re_mpi_gdc.h
@@ -7,30 +7,33 @@
 
 #include "mpi_gdc.h"
 #include "mpi_errno.h"
+#include <sys/ioctl.h>
 
-
-typedef struct hiGDC_DATA_S { // s:0x12
-    SIZE_S stSize;
-    HI_U32 u32Range;
-} GDC_DATA_S;
-
-// GDC_CFG_U1_S; - FISHEYE_REGION_ATTR_EX_S or FISHEYE_REGION_ATTR_S
-
-typedef struct hiGDC_CFG_U2_S { // s:0x170
-    HI_U32 field_0[92];
-} GDC_CFG_U2_S;
-
-typedef struct hiGDC_PMF_TASK_INFO_S { // s:0x540
-    GDC_HANDLE hHandle;
-    HI_U32 field_4;
-    GDC_TASK_ATTR_S stTask;
-    HI_U32 field_2D0;
-    HI_U32 field_2D4;
-    GDC_PMF_ATTR_S stGdcPmfAttr;
-    HI_U32 field_320[136];
+typedef struct hiGDC_PMF_TASK_INFO_S { // (sizeof=0x540)
+    GDC_HANDLE hHandle;          // 0x000
+    HI_U32 field_4;              // 0x004
+    GDC_TASK_ATTR_S stTask;      // 0x008
+    HI_U32 field_2D0;            // 0x2D0
+    HI_U32 field_2D4;            // 0x2D4
+    GDC_PMF_ATTR_S stGdcPmfAttr; // 0x2D8
+    HI_U32 field_320[136];       // 0x320
 } GDC_PMF_TASK_INFO_S;
 
+// typedef struct hiGDC_DATA_S { // s:0x12
+//     SIZE_S stSize;
+//     HI_U32 u32Range;
+// } GDC_DATA_S;
 
-HI_S32 mpi_gdc_check_open(HI_VOID);
+// // GDC_CFG_U1_S; - FISHEYE_REGION_ATTR_EX_S or FISHEYE_REGION_ATTR_S
+
+// typedef struct hiGDC_CFG_U2_S { // s:0x170
+//     HI_U32 field_0[92];
+// } GDC_CFG_U2_S;
+
+#define IOC_TYPE_GDC 'N' // 0x4E
+#define IOC_GDC_BEGIN_JOB    _IOW(IOC_TYPE_GDC, 0x00, GDC_HANDLE         ) /* 0x80044E00u */
+#define IOC_GDC_CANCEL_JOB   _IOR(IOC_TYPE_GDC, 0x01, GDC_HANDLE         ) /* 0x40044E01u */
+#define IOC_GDC_END_JOB      _IOR(IOC_TYPE_GDC, 0x02, GDC_HANDLE         ) /* 0x40044E02u */
+#define IOC_GDC_ADD_PMF_TASK _IOR(IOC_TYPE_GDC, 0x05, GDC_PMF_TASK_INFO_S) /* 0x45404E05u */
 
 #endif
